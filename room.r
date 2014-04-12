3d13eee9ed74f0a768d460cec0b7ccc9a916f9ec
briholt100 <brian.c.holt@gmail.com>
2014-04-11 17:38:45 -0700

    added some hypotheses

--- room.r
+++ room.r
@@ -2,3 +2,9 @@ getwd()
 room<-read.csv("SpAll4-9.csv")
 head(room)
 str(room)
+
+##one prediction is that type of class should predict room
+
+## a different prediction would be that a department would predict a particular room
+
+## yet another prediction about which rooms are most empty?  Hybrids.  But what of other designations?
