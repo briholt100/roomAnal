78bc6ca7ea386cd4c2b937da7f5bd2d26c739077
Brian Holt <brian.c.holt@gmail.com>
2014-04-11 20:57:17 -0700

    added some tables, renamed to .R

---
+++ room.R
@@ -0,0 +1,13 @@
+getwd()
+room<-read.csv("SpAll4-9.csv")
+head(room)
+str(room)
+table(room$DEPT,room$BLDG)
+t(table(room$DAYS))
+##one prediction is that type of class should predict room
+
+## a different prediction would be that a department would predict a particular room
+
+## yet another prediction about which rooms are most empty?  Hybrids.  But what of other designations?
+
+
